3e687e57cc7ec7162cd836ae2435c4874ddc3369
shareef-dweikat <shareef.dweikat@gmail.com>
2021-07-31 16:04:51 +0300

    description component refactored into Heading component. Started using constants

--- lib/widgets/Heading.dart
+++ lib/widgets/Heading.dart
@@ -3,16 +3,16 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DescriptionContainer extends StatelessWidget {
   //this component needs a lot of enhancements
-  final String title;
-  final TextAlign textAlign;
-  final double marginTop;
-  final double fontSize;
-  DescriptionContainer(this.title, this.textAlign, this.marginTop, this.fontSize, Key key):super(key:key);
-
+   String? title;
+   TextAlign? textAlign;
+   double? marginTop;
+   double? fontSize;
+  // DescriptionContainer(this.title, this.textAlign, this.marginTop, this.fontSize, Key key):super(key:key);
+  DescriptionContainer({Key? key, String? title, TextAlign? textAlign, double? marginTop, double? fontSize}):super(key:key);
   @override
   Widget build(BuildContext context) {
     return  Container(
-        margin: EdgeInsets.only(top:marginTop),
+        margin: EdgeInsets.only(top:marginTop!),
           child: Text('${title}',
             textAlign: textAlign,
             style:TextStyle(color: Colors.white, fontSize: fontSize,),
