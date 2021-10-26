8bf3d0dbfffa9f8b034390ebada4312c17fa3e4a
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-26 16:42:03 +0200

    add routes constants
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,6 @@
+const String HOME_ROUTE = '/home';
+const String INTRO_ROUTE = '/intro';
+const String SEND_ROUTE = '/send';
+const String RECEIVE_ROUTE = '/receive';
+const String SPLASH_ROUTE = '/';
+const String SETTINGS_ROUTE = '/settings';
