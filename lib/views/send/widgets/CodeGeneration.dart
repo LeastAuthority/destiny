5b93792e934d776b25466e6f81fb915e36ae1e2e
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-26 17:05:44 +0200

    add CodeGeneration
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/views/send/widgets/CodeGeneration.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:dart_wormhole_gui/constants/app_constants.dart';
+import 'package:dart_wormhole_gui/views/send/widgets/RowGroupButtons.dart';
+import 'package:dart_wormhole_gui/widgets/buttons/Button.dart';
+import 'package:dart_wormhole_gui/widgets/FileInfo.dart';
+import 'package:dart_wormhole_gui/widgets/Heading.dart';
+
+class CodeGeneration extends StatelessWidget {
+  String? fileName = '';
+  int? fileSize = 0;
+  String code = '';
+  CodeGeneration({Key? key, String? fileName, int? fileSize, String code = ''}):super(key:key) {
+    this.fileName = fileName;
+    this.fileSize = fileSize;
+    this.code = code;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        FileInfo(fileSize, fileName),
+        RowGroupButton(GENERATION_CODE, code),
+        Heading(
+            title: SHARE_CODE_WITH_RECIPIENT_AND_WAIT_UNTIL_THE_TRANSFER_IS_COMPLETE,
+            textAlign:TextAlign.center,
+            marginTop: 16.0.h,
+            textStyle: Theme.of(context).textTheme.bodyText2,
+          key:Key(GENERATION_DESCRIPTION),
+        ),
+        Button(CANCEL, () {
+        })
+      ],
+    );
+  }
+}
