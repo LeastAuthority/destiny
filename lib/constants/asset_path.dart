069ce4aee819c721510b661b6661b603a306e42a
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-26 16:58:22 +0200

    add assets
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/constants/asset_path.dart
@@ -0,0 +1,16 @@
+//fonts
+final String MONTSERRAT = 'Montserrat';
+final String ROBOTO = 'Roboto';
+final String LATO = 'Lato';
+
+//images
+final String LOGO = 'assets/images/logo.png';
+
+//Icons
+final String SEND_ICON = 'assets/images/icons/send.png';
+final String RECEIVE_ICON = 'assets/images/icons/receive.png';
+final String SETTINGS_ICON = 'assets/images/icons/SETTINGS-WHITE.png';
+final String SETTINGS_ICON_WITH_CIRCLE = 'assets/images/icons/SETTINGS-WHITE-with-circle.png';
+final String PHONE_ICON = 'assets/images/icons/DEVICEWHITE.png';
+final String COPY_ICON = 'assets/images/icons/Paste-white.png';
+final String BURGER_ICON = 'assets/images/icons/Hamburger-Menu-White.png';
