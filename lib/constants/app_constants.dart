d6abfe4d90b701a41d5bed60b17ef88d48678077
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-26 16:40:37 +0200

    add app_contants
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,64 @@
+//screens titles
+final String HOME = "Home";
+final String SEND = "Send";
+final String RECEIVE = "Receive";
+final String SETTINGS = "Settings";
+
+//ui strings
+final String SEND_AND_RECEIVE_FILES_SECURLY_AND_FAST = 'Send and receive files securely and fast';
+final String ENTER_THE_CODE_IN_ORDER_TO_RECEIVE_THE_FILE = 'Enter the code in order to receive the file.';
+final String SHARE_CODE_WITH_RECIPIENT_AND_WAIT_UNTIL_THE_TRANSFER_IS_COMPLETE = 'Share code with recipient & wait until the transfer is complete.';
+final String SELECT_A_FILE = 'Select a File';
+final String SELECT_A_FOLDER = 'Select a Folder';
+final String NEXT = 'Next';
+final String CANCEL = 'Cancel';
+final String BACK = 'Back';
+final String SELECT_DEFAULT_SAVE_DESTINATION_FOR_THIS_DEVICE = 'Select default save destination for this device.';
+final String CURRENT_SAVE_DESTINATION = 'Current save destination:';
+final String GENERATION_CODE = 'Generating Code';
+final String SEEN= 'seen';
+final String LOADING= 'Loading...';
+final String CODE_COPIED = 'Code Copied';
+final String CUSTOM_NAV_BAR_MENU_FANDQ_ITEM = 'F&Q';
+//keys for tests
+//-General tests
+final String BOTTOM_NAV_BAR = 'BOTTOM_NAV_BAR';
+final String BOTTOM_NAV_BAR_BODY = 'BOTTOM_NAV_BAR_BODY';
+final String BOTTOM_NAV_BAR_CONTAINER = 'BOTTOM_NAV_BAR_CONTAINER';
+final String BOTTOM_NAV_BAR_LEFT_ITEM = 'BOTTOM_NAV_BAR_LEFT_ITEM';
+final String BOTTOM_NAV_BAR_RIGHT_ITEM = 'BOTTOM_NAV_BAR_RIGHT_ITEM';
+//-Navbar tests
+final String CUSTOM_NAV_BAR = 'CUSTOM_NAV_BAR';
+final String CUSTOM_NAV_BAR_BODY = 'CUSTOM_NAV_BAR_BODY';
+final String CUSTOM_NAV_BAR_CONTAINER = 'CUSTOM_NAV_BAR_CONTAINER';
+final String CUSTOM_NAV_BAR_LEFT_ITEM = 'CUSTOM_NAV_BAR_LEFT_ITEM';
+final String CUSTOM_NAV_BAR_MIDDLE_ITEM = 'CUSTOM_NAV_BAR_MIDDLE_ITEM';
+final String CUSTOM_NAV_BAR_RIGHT_ITEM = 'CUSTOM_NAV_BAR_RIGHT_ITEM';
+//-Send screen
+final String SEND_SCREEN_BODY = 'SEND_SCREEN_BODY';
+final String SEND_SCREEN_CONTENT = 'SEND_SCREEN_CONTENT';
+final String SEND_SCREEN_HEADING = 'SEND_SCREEN_HEADING';
+final String SEND_SCREEN_BOTTOM_SPACE_PLACEHOLDER = 'SEND_SCREEN_BOTTOM_SPACE_PLACEHOLDER';
+final String SEND_SCREEN_CODE_GENERATION_UI = 'SEND_SCREEN_CODE_GENERATION_UI';
+final String SEND_SCREEN_SELECT_A_FILE_BUTTON = 'SEND_SCREEN_SELECT_A_FILE_BUTTON';
+final String GENERATION_DESCRIPTION = 'Generation_Description';
+
+//-Receive screen
+final String RECEIVE_SCREEN_BODY = 'RECEIVE_SCREEN_BODY';
+final String RECEIVE_SCREEN_CONTENT = 'RECEIVE_SCREEN_CONTENT';
+final String RECEIVE_SCREEN_HEADING = 'RECEIVE_SCREEN_HEADING';
+final String RECEIVE_SCREEN_ENTER_CODE= 'RECEIVE_SCREEN_ENTER_CODE';
+
+//-Splash screen
+final String SPLASH_SCREEN_BODY= 'SPLASH_SCREEN_BODY';
+final String SPLASH_SCREEN_LOADING= 'SPLASH_SCREEN_LOADING';
+
+
+//-Settings screen
+final String SETTINGS_SCREEN_BODY = 'SETTINGS_SCREEN_BODY';
+final String SETTINGS_SCREEN_CONTENT = 'SETTINGS_SCREEN_CONTENT';
+final String SETTINGS_SCREEN_HEADING = 'SETTINGS_SCREEN_HEADING';
+final String SETTINGS_SCREEN_SELECT_A_FOLDER_BUTTON = 'SETTINGS_SCREEN_SELECT_A_FOLDER_BUTTON';
+final String SETTINGS_SCREEN_BOTTOM_SPACE_PLACEHOLDER = 'SETTINGS_SCREEN_BOTTOM_SPACE_PLACEHOLDER';
+
+
