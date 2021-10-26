1bcefda8c9cd844ea2101ab50c62dbd107d0bbd5
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-26 17:13:38 +0200

    add SendingProgress send widget
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/views/send/widgets/SendingProgress.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:dart_wormhole_gui/widgets/buttons/Button.dart';
+import 'package:dart_wormhole_gui/widgets/FileInfo.dart';
+import 'package:dart_wormhole_gui/widgets/Heading.dart';
+
+class SendingProgress extends StatelessWidget {
+  final int fileSize;
+  final String fileName;
+  SendingProgress(this.fileSize, this.fileName);
+  @override
+  Widget build(BuildContext context) {
+    return   Column(
+      children: [
+        FileInfo(fileSize, fileName),
+        Container(
+          margin: EdgeInsets.only(top: 32.0.h),
+          child: LinearProgressIndicator(
+            value: 0.5,
+          ),
+        ),
+        Heading(
+          title: '2 Seconds',
+          textAlign: TextAlign.left,
+          marginTop:16.0.h,
+          textStyle: Theme.of(context).textTheme.bodyText2,
+          key: Key('Timing_Progress'),
+        ),
+        Heading(
+          title: 'App must remain open until the transfer is complete.',
+          textAlign: TextAlign.center,
+          marginTop:16.0.h,
+          textStyle: Theme.of(context).textTheme.bodyText1,
+          key: Key('APP_MUST_REMAIN_OPEN'),
+        ),
+        Button('Cancel', () {})
+      ],
+    );
+  }
+}
