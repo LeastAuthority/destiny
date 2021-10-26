4b4e7485d6648d670e8e0f750f18df62ec548755
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-26 17:44:09 +0200

    add custom bottom bar widget test
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'package:dart_wormhole_gui/config/routes/routes.dart';
 import 'package:dart_wormhole_gui/constants/app_constants.dart';
 import 'package:dart_wormhole_gui/views/settings.dart';
 import 'package:dart_wormhole_gui/views/splash.dart';
+import 'package:dart_wormhole_gui/widgets/custom-bottom-bar.dart';
 
  void main() {
    testWidgets('Splash screen', (WidgetTester tester) async {
@@ -32,5 +34,26 @@ import 'package:dart_wormhole_gui/views/splash.dart';
      final settingsScreenBody = find.byKey(Key(SETTINGS_SCREEN_BODY));
 
      expect(settingsScreenBody, findsOneWidget);
+   });
+
+   testWidgets('Custom BottomBar', (WidgetTester tester) async {
+     await tester.pumpWidget(
+         MaterialApp(
+             home: Scaffold(
+                 bottomNavigationBar: CustomBottomBar(
+                 path: SEND_ROUTE,
+                 key: Key(BOTTOM_NAV_BAR),
+               ),
+             ))
+     );
+     final bottomBarBody = find.byKey(Key(BOTTOM_NAV_BAR_BODY));
+     final bottomNavbarContainer = find.byKey(Key(BOTTOM_NAV_BAR_CONTAINER));
+     final bottomNavbarLeftItem = find.byKey(Key(BOTTOM_NAV_BAR_LEFT_ITEM));
+     final bottomNavbarRightItem = find.byKey(Key(BOTTOM_NAV_BAR_RIGHT_ITEM));
+
+     expect(bottomBarBody, findsOneWidget);
+     expect(bottomNavbarContainer, findsOneWidget);
+     expect(bottomNavbarLeftItem, findsOneWidget);
+     expect(bottomNavbarRightItem, findsOneWidget);
    });
 }
