8ced645102ee6c05789f43a923876dc1d1b3a2b6
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-26 17:08:13 +0200

    add FileInfo widget
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/widgets/FileInfo.dart
@@ -0,0 +1,40 @@
+import 'package:dart_wormhole_gui/constants/asset_path.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:dart_wormhole_gui/widgets/Heading.dart';
+
+class FileInfo extends StatelessWidget {
+  final int? fileSize;
+  final String? fileName;
+
+  FileInfo(this.fileSize, this.fileName);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 32.0.h),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text('${fileName}',
+              style:TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 17.sp,
+                  fontFamily: ROBOTO,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        ),
+        Heading(
+          title: '(${fileSize} kb)',
+          textAlign: TextAlign.center,
+          marginTop:0,
+          textStyle: Theme.of(context).textTheme.bodyText2,
+          key: Key('File_Info_Description'),
+        ),
+      ],
+    );
+  }
+}
