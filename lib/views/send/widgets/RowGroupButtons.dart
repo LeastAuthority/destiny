dece232d22f95fbefcab1d0074c4ecaaa351b851
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-27 09:28:29 +0200

    add RowGroupButton widget
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>
    
    
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/views/send/widgets/RowGroupButtons.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:dart_wormhole_gui/constants/app_constants.dart';
+import 'package:dart_wormhole_gui/constants/asset_path.dart';
+import 'package:dart_wormhole_gui/widgets/buttons/ButtonLinearGradientWithIcon.dart';
+import 'package:dart_wormhole_gui/widgets/buttons/ButtonWithIcon.dart';
+
+class RowGroupButton extends StatelessWidget {
+  final String label;
+  String code = '';
+  RowGroupButton(this.label, this.code);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ButtonLinearGradientWithIcon(
+            label: code,
+            handleSelectFile: () {},
+            icon: CircularProgressIndicator(
+              value: 1,
+              semanticsLabel: 'Linear progress indicator',
+            ),
+          height: 70.0,
+          width: 190.0,
+          isVertical: false,
+        ),
+        SizedBox(
+          width: 8.0.w,
+        ),
+        ButtonWithIcon(
+          label:'Copy',
+          handleSelectFile: () {
+            Clipboard.setData(ClipboardData(text: code));
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(CODE_COPIED),
+            ));
+          },
+          icon:Image.asset(
+            COPY_ICON,
+            width: 30.0.w,
+          ),
+          height:70.0,
+          width: 70.0,
+          isVertical: true,
+        ),
+      ],
+    );
+  }
+}
