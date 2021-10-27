2389ee5bc54f42fd10709067d3624b963e05df7c
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-10-27 10:54:47 +0200

    add custom theme and colors
    Co-authored-by: Shareef Dweikat <shareef.dweikat@gmail.com>
    Co-authored-by: Bryan White <bryanchriswhite@gmail.com>

---
+++ lib/config/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color darkBlue = Color(0xff1A1C2E);
+  static const Color purple = Color(0xffC24DF8);
+  static const Color lightPurple = Color(0xffc98ae6);
+  static const Color lightBlue = Color(0xff33B6FF);
+  static const Color babyPowder = Color(0xFFFFFFFC);
+}
