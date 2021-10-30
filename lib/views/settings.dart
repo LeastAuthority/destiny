15f7ad22d09ef85b919ac8920aff63d1e5f59d4d
shareef-dweikat <shareef.dweikat@gmail.com>
2021-10-30 16:23:22 +0300

    settings headding

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -32,9 +32,11 @@ class Settings extends StatelessWidget {
                         Column(
                           children: [
                             Heading(
-                              title: '${SELECT_DEFAULT_SAVE_DESTINATION_FOR_THIS_DEVICE} ${CURRENT_SAVE_DESTINATION}',
+                              title:
+                              '${SELECT_DEFAULT_SAVE_DESTINATION_FOR_THIS_DEVICE} '
+                                  '${CURRENT_SAVE_DESTINATION}',
                               textAlign: TextAlign.left,
-                              marginTop:0,
+                              marginTop: 0,
                               path: 'Destiny/Received',
                               textStyle: Theme.of(context).textTheme.bodyText1,
                               key: Key(SETTINGS_SCREEN_HEADING),
