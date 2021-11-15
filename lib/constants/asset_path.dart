8b2823b18e56cd867fa508329e9c795f5d78b508
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-11-15 17:16:45 +0200

    added plus icon and dotted border dependancy

--- lib/constants/asset_path.dart
+++ lib/constants/asset_path.dart
@@ -12,5 +12,6 @@ final String RECEIVE_ICON = 'assets/images/icons/receive.png';
 final String SETTINGS_ICON = 'assets/images/icons/SETTINGS-WHITE.png';
 final String SETTINGS_ICON_WITH_CIRCLE = 'assets/images/icons/SETTINGS-WHITE-with-circle.png';
 final String PHONE_ICON = 'assets/images/icons/DEVICEWHITE.png';
+final String PLUS_ICON = 'assets/images/icons/Plus-Button-Large.png';
 final String COPY_ICON = 'assets/images/icons/Paste-white.png';
 final String BURGER_ICON = 'assets/images/icons/Hamburger-Menu-White.png';
