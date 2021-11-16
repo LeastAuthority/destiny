4cb5e6a5f676b1148ba38552b4a0ffddee11f3a1
Shareef Dweikat <shareef.dweikat@gmail.com>
2021-11-16 19:59:43 +0200

    Added desktop_drop and upgraded file_picker dependencies

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <desktop_drop/desktop_drop_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) desktop_drop_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DesktopDropPlugin");
+  desktop_drop_plugin_register_with_registrar(desktop_drop_registrar);
 }
