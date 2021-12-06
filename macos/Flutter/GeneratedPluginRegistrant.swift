e7b5301672969d651431a8d669cf98a34d35c75d
Shareef Dweikat <shareef-dweikat@users.noreply.github.com>
2021-12-06 13:20:26 +0200

    Shareef/send code received (#39)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import dart_wormhole_william
 import desktop_drop
 import desktop_window
-import path_provider_macos
 import shared_preferences_macos
 import window_size
 
@@ -16,7 +15,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DartWormholeWilliamPlugin.register(with: registry.registrar(forPlugin: "DartWormholeWilliamPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
