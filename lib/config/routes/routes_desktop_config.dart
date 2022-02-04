ec4b3168ddf50bdcf3c20b3f0b282bb21bd24b29
shareef-dweikat <shareef.dweikat@gmail.com>
2022-02-04 12:57:59 +0200

    use desktop widgets for desktop

--- lib/config/routes/routes_desktop_config.dart
+++ lib/config/routes/routes_desktop_config.dart
@@ -1,9 +1,7 @@
 import 'package:dart_wormhole_gui/config/routes/routes.dart';
 import 'package:dart_wormhole_gui/views/desktop/send/send.dart';
-
-// TODO temporarily using mobile screens for desktop
-import 'package:dart_wormhole_gui/views/mobile/receive/receive.dart';
-import 'package:dart_wormhole_gui/views/mobile/settings.dart';
+import 'package:dart_wormhole_gui/views/desktop/receive/receive.dart';
+import 'package:dart_wormhole_gui/views/desktop/settings.dart';
 import 'package:flutter/cupertino.dart';
 
 PageRouteBuilder? getDesktopRoutes(RouteSettings settings) {
@@ -12,17 +10,14 @@ PageRouteBuilder? getDesktopRoutes(RouteSettings settings) {
       {
         return PageRouteBuilder(pageBuilder: (_, __, ___) => Send());
       }
-      break;
     case RECEIVE_ROUTE:
       {
         return PageRouteBuilder(pageBuilder: (_, __, ___) => Receive());
       }
-      break;
     case SETTINGS_ROUTE:
       {
         return PageRouteBuilder(pageBuilder: (_, __, ___) => Settings());
       }
-      break;
   }
   return null;
 }
