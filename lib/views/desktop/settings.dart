70d4a25b4fff532bf4b97e6b55bc6da70897ab32
shareef-dweikat <shareef.dweikat@gmail.com>
2022-02-05 18:48:31 +0200

    build desktop settings screen

--- lib/views/desktop/settings.dart
+++ lib/views/desktop/settings.dart
@@ -1,11 +1,21 @@
 import 'package:dart_wormhole_gui/config/routes/routes.dart';
 import 'package:dart_wormhole_gui/config/theme/colors.dart';
 import 'package:dart_wormhole_gui/constants/app_constants.dart';
+import 'package:dart_wormhole_gui/views/desktop/widgets/DTButtonWithBackground.dart';
 import 'package:dart_wormhole_gui/views/desktop/widgets/custom-app-bar.dart';
+import 'package:dart_wormhole_gui/views/widgets/Heading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:dart_wormhole_gui/views/shared/settings.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends SettingsState {
+  Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends SettingsShared<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,9 +45,38 @@ class Settings extends StatelessWidget {
                 child: Container(
                     height: double.infinity,
                     margin: EdgeInsets.fromLTRB(16.0.w, 0.0, 16.0.w, 4.0.w),
+                    padding: EdgeInsets.only(top: 80.0.h),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [],
+                      children: [
+                        Heading(
+                          title: SELECT_DEFAULT_SAVE_DESTINATION_FOR_THIS_DEVICE,
+                          textAlign: TextAlign.center,
+                          marginTop: 16.0.h,
+                          textStyle: Theme.of(context).textTheme.headline1,
+                          // key: Key('Timing_Progress'),
+                        ),
+                       Column(
+                         children: [
+                           Heading(
+                             title:
+                             '$CURRENT_SAVE_DESTINATION',
+                             textAlign: TextAlign.center,
+                             marginTop: 110.0.h,
+                             path: 'destiny/received',
+                             textStyle: Theme.of(context).textTheme.headline1,
+                             key: Key(SETTINGS_SCREEN_HEADING),
+                           ),
+                           SizedBox(
+                             height: 45.0.h,
+                           ),
+                           DTButtonWithBackground(
+                             handleSelectFile: handleSelectFile,
+                             title: SELECT_A_FOLDER,
+                             width: 140.0.w,
+                           )
+                         ],
+                       )
+                      ],
                     )),
               ),
             )));
