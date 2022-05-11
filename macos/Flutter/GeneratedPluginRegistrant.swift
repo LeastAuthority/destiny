806335b52b4c9fc302c15c46da089780e7c7bd41
shareef-dweikat <shareef.dweikat@gmail.com>
2022-05-11 17:27:33 +0300

    fix conflict

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import dart_wormhole_william
 import desktop_drop
 import path_provider_macos
 import shared_preferences_macos
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DartWormholeWilliamPlugin.register(with: registry.registrar(forPlugin: "DartWormholeWilliamPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
