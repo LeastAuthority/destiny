67365482ed1849fc5fe5a935520af3f9b9dcc32e
El-Hassan Wanas <wanas@leastauthority.com>
2022-08-31 12:02:36 +0300

    Minor cleanup

--- lib/views/shared/settings.dart
+++ lib/views/shared/settings.dart
@@ -52,18 +52,17 @@ abstract class SettingsShared<T extends SettingsState> extends State<T> {
       if (directory == null) {
         return;
       }
-      await canWriteToDirectory(directory).then((canWrite) async {
-        if (canWrite) {
-          setState(() {
-            prefs?.setString(PATH, "$directory${Platform.pathSeparator}");
-          });
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: Text(
-                THE_APP_DOES_NOT_HAVE_THE_PREMISSION_TO_STORE_FILES_IN_THE_DIR),
-          ));
-        }
-      });
+
+      if (await canWriteToDirectory(directory)) {
+        setState(() {
+          prefs?.setString(PATH, "$directory${Platform.pathSeparator}");
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+              THE_APP_DOES_NOT_HAVE_THE_PREMISSION_TO_STORE_FILES_IN_THE_DIR),
+        ));
+      }
     } finally {
       this.setState(() {
         selectingFolder = false;
