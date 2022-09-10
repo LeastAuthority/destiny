ac4997c00f1c1483df9dffd60ffadcc65b601386
shareef-dweikat <shareef.dweikat@gmail.com>
2022-09-10 13:54:39 +0300

    Prevent dropping a directory

--- lib/views/desktop/send/widgets/DTSelectOrDropAFile.dart
+++ lib/views/desktop/send/widgets/DTSelectOrDropAFile.dart
@@ -1,16 +1,17 @@
+import 'package:dart_wormhole_william/client/file.dart';
 import 'package:destiny/views/desktop/send/widgets/DTSelectAFile.dart';
 import 'package:destiny/views/shared/util.dart';
 import 'package:dart_wormhole_william/client/file.dart' as f;
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../constants/app_constants.dart';
 import 'DTDropAFile.dart';
 
 class DTSelectOrDropAFile extends StatefulWidget {
   final Future<void> Function() onFileSelected;
   final Future<void> Function(f.File) onFileDropped;
   bool dragEntered = false;
-  bool dragDone = false;
   DTSelectOrDropAFile(
       {Key? key, required this.onFileSelected, required this.onFileDropped})
       : super(key: key);
@@ -22,34 +23,32 @@ class DTSelectOrDropAFile extends StatefulWidget {
 class _DTSelectOrDropAFile extends State<DTSelectOrDropAFile> {
   @override
   Widget build(BuildContext context) {
-    bool x = false;
+    bool flag = false;
     return Container(
       color: widget.dragEntered
           ? Color(0xff3A2655)
           : Theme.of(context).dialogBackgroundColor,
       child: DropTarget(
         onDragDone: (detail) async {
-          if(x== true) return;
-          print('dddddddd');
+          if (flag == true) return;
           try {
-            await widget.onFileDropped(detail.files.first.readOnlyFile());
+            File file = detail.files.first.readOnlyFile();
+            await widget.onFileDropped(file);
+            flag = true;
           } catch (e) {
-            bool isFile = detail.files.first.path.split('.').length <= 1;
+            bool isFile = detail.files.first.path.split('.').length == 0;
+            flag = true;
             if (isFile) return;
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('Directories are not allowed'),
+              content: Text(DIRECTORIES_ARE_NOT_ALLOWED),
             ));
           }
-          x = true;
-          // this.setState(() {
-          //   widget.dragDone = true;
-          // });
+          flag = true;
         },
         onDragEntered: (detail) async {
-          x = false;
+          flag = false;
           this.setState(() {
             widget.dragEntered = true;
-            // widget.dragDone = false;
           });
         },
         onDragExited: (detail) async {
