4c08056ff729c43fb9e5095c10b1dce0421be85a
El-Hassan Wanas <wanas@leastauthority.com>
2022-09-16 12:11:25 +0300

    Update dependencies and remove unused ones. Pin git dependencies.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import dart_wormhole_william
 import desktop_drop
-import device_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
 import url_launcher_macos
@@ -16,7 +15,6 @@ import window_size
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DartWormholeWilliamPlugin.register(with: registry.registrar(forPlugin: "DartWormholeWilliamPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
