cf355c44a5f8525331160afb90fed2137a54861f
Donatas Puidokas <104410044+donpui@users.noreply.github.com>
2023-01-30 08:31:04 +0200

    Fix lint issue

--- lib/views/shared/receive.dart
+++ lib/views/shared/receive.dart
@@ -11,7 +11,6 @@ import 'package:dart_wormhole_william/client/native_client.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart' as f;
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wakelock/wakelock.dart';
 
